e629d3e778e3fe8fec13e18dbb102bbb1e82757c
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-05 14:11:58 -0300

    test: Retornando mensagem de erro quando não informar o usuário

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    public function makeUser() {
+        return [
+            'name' => 'Test User',
+            'email' => 'test@test.com',
+            'email_confirm' => 'test@test.com',
+            'password' => bcrypt('secret'),
+            'password_confirm' => bcrypt('secret'),
+        ];
+    }
+    /**
+     * @test
+     */
+
+     public function name_should_be_required() {
+        $user = $this->makeUser();
+        $user['name'] = '';
+        $return =  $this->post(route('auth.register'), [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'password' => $user['password'],
+
+        ]);
+        $return->assertSessionHasErrors([
+            'name' => 'O Nome de usuário é obrigatório'
+        ]);
+     }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+    // use DatabaseTransactions;
+    
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('passport:install');
+    }
 }
