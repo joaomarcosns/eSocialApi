97d7aeabe8d389a6a90c8a92eda856c93cbf261e
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-05 17:41:25 -0300

    test: Retornando mensagem de erro quando não informa a senha

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -54,5 +54,24 @@ class LoginTest extends TestCase
             'email' => 'O campo de e-mail deve ser do tipo e-mail'
         ]);
     }
+    /**
+     * @test
+     */
+    public function password_should_be_required()
+    {
+        $user = $this->makeUser();
+        $user['password'] = '';
+        $return =  $this->post(route('auth.login'), [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'email' => $user['email'],
+            'password' => $user['password'],
+
+        ]);
+        $return->assertStatus(302);
+        $return->assertSessionHasErrors([
+            'password' => 'O campo senha é obrigatório'
+        ]);
+    }
 
 }
