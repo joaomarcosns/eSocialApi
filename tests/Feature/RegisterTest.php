9fdb924a3d89062ba21fd6d1f095ce93bed8c360
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-05 15:03:14 -0300

    test: Retornando mensagem de erro quando não informar o e-mail do usuário

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -54,9 +54,31 @@ class RegisterTest extends TestCase
         ]);
         $return->assertStatus(302);
         $return->assertSessionHasErrors([
-            'name' => 'O Nome de usuário apenas pode ter max characters'
+            'name' => 'O Nome de usuário apenas pode ter 255 characters'
         ]);
     }
+    
+    /**
+     * @test
+     */
+    public function name_should_have_a_min_of_3_characterisers()
+    {
+        $user = $this->makeUser();
+        $user['name'] = str_repeat('a', 2);
+        $return =  $this->post(route('auth.register'), [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'password' => $user['password'],
+
+        ]);
+        $return->assertStatus(302);
+        $return->assertSessionHasErrors([
+            'name' => 'O Nome de usuário tem ser maior que 3 characters'
+        ]);
+    }
+
     /**
      * @test
      */
@@ -75,6 +97,26 @@ class RegisterTest extends TestCase
         $return->assertStatus(302);
         $return->assertSessionHasErrors([
             'name' => 'Não pode conter números nesse campo'
+        ]);
+    }
+    /**
+     * @test
+     */
+    public function email_should_be_required()
+    {
+        $user = $this->makeUser();
+        $user['email'] = '';
+        $return =  $this->post(route('auth.register'), [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'password' => $user['password'],
+
+        ]);
+        $return->assertStatus(302);
+        $return->assertSessionHasErrors([
+            'email' => 'O campo de e-mail é obrigatório'
         ]);
     }
 }
