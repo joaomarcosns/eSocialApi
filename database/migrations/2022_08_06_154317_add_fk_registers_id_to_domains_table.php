fe8b87c10c7c06533be1f06b16cee1451ad5065e
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-06 14:01:50 -0300

    refactor: refatorando regra de negocio para importação

--- database/migrations/2022_08_06_154317_add_fk_registers_id_to_domains_table.php
+++ database/migrations/2022_08_06_154317_add_fk_registers_id_to_domains_table.php
@@ -3,12 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
-<<<<<<<< HEAD:database/migrations/2022_08_06_154317_add_fk_registers_id_to_domains_table.php
 class AddFkRegistersIdToDomainsTable extends Migration
-========
-class AddFkRegistersIdToDomaisTable extends Migration
->>>>>>>> e2f03a640ac50b59be5ce7a6c6877b9957393149:database/migrations/2022_08_06_153519_add_fk_registers_id_to_domais_table.php
 {
     /**
      * Run the migrations.
@@ -17,11 +12,7 @@ class AddFkRegistersIdToDomaisTable extends Migration
      */
     public function up()
     {
-<<<<<<<< HEAD:database/migrations/2022_08_06_154317_add_fk_registers_id_to_domains_table.php
         Schema::table('domains', function (Blueprint $table) {
-========
-        Schema::table('domais', function (Blueprint $table) {
->>>>>>>> e2f03a640ac50b59be5ce7a6c6877b9957393149:database/migrations/2022_08_06_153519_add_fk_registers_id_to_domais_table.php
             $table->unsignedBigInteger('fk_registers_id');
             $table->foreign('fk_registers_id')->references('id')->on('registers');
         });
@@ -34,11 +25,7 @@ class AddFkRegistersIdToDomaisTable extends Migration
      */
     public function down()
     {
-<<<<<<<< HEAD:database/migrations/2022_08_06_154317_add_fk_registers_id_to_domains_table.php
         Schema::table('domains', function (Blueprint $table) {
-========
-        Schema::table('domais', function (Blueprint $table) {
->>>>>>>> e2f03a640ac50b59be5ce7a6c6877b9957393149:database/migrations/2022_08_06_153519_add_fk_registers_id_to_domais_table.php
             $table->dropColumn('fk_registers_id');
         });
     }
