057f831597c49a850b268bd8298e24b3f3df36e0
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-13 15:36:09 -0300

    test: Retornando mensagem de erro, quando nao informar o arquivo

--- tests/Feature/DomainsTest.php
+++ tests/Feature/DomainsTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 use Laravel\Passport\Passport;
 
@@ -22,6 +23,27 @@ class DomainsTest extends TestCase
         ]);
         $return->assertJson([
             'message' => 'Unauthenticated.'
+        ]);
+    }
+    /**
+     * @test
+     */
+    public function file_should_be_required()
+    {
+        Passport::actingAs(
+            $user  = User::factory()->createOne(),
+            ['create-servers']
+        );
+        $return =  $this->post(route('domains.upload'), [
+            'Accept' => 'application/json',
+            'Content-Type' => 'multipart/form-data',
+            'Authorization' => "Bearer {$user->createToken('Personal Access Token')->accessToken}",
+            'file' => ''
+        ]);
+
+        $return->assertStatus(302);
+        $return->assertSessionHasErrors([
+            'file' => 'O arquivo é obrigatório'
         ]);
     }
 }
