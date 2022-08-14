d224e7f003cbfe98ac25da382ebba0c0a961c1c2
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-14 15:10:39 -0300

    feat: Retornando mensagem de erro quando  não informar  o primeiro name server

--- app/Http/Requests/DomainsStoreRequest.php
+++ app/Http/Requests/DomainsStoreRequest.php
@@ -29,6 +29,7 @@ class DomainsStoreRequest extends FormRequest
             'register' => ['required', 'max:255'],
             'created_at' => ['required'],
             'updated_at' => ['required'],
+            'nameserver_1' => ['required'],
         ];
     }
 
@@ -43,7 +44,8 @@ class DomainsStoreRequest extends FormRequest
             'register.required' => 'O nome do registrador é obrigatório',
             'register.max' => 'O tamanho máximo do nome do registrador é :max',
             'created_at.required' => 'O campo é obrigatório',
-            'updated_at.required' => 'O campo é obrigatório'
+            'updated_at.required' => 'O campo é obrigatório',
+            'nameserver_1.required' =>'O campo é obrigatório'
         ];
     }
 }
