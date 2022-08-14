b586845dc1485e0a71b5ae71c3d731694c54fb0f
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-08-14 15:08:48 -0300

    test: Retornando mensagem de erro quando  não informar  a data de atualização

--- tests/Feature/DomainsTest.php
+++ tests/Feature/DomainsTest.php
@@ -321,4 +321,32 @@ class DomainsTest extends TestCase
             'created_at' => 'O campo é obrigatório'
         ]);
     }
+    /**
+     * @test
+     */
+    public function domains_updated_at_should_be_required()
+    {
+        Passport::actingAs(
+            $user  = User::factory()->createOne(),
+            ['create-servers']
+        );
+        $domain = $this->makeDomains();
+        $domain['updated_at'] = '';
+        $return =  $this->post(route('domains.store'), [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'Authorization' => "Bearer {$user->createToken('Personal Access Token')->accessToken}",
+            "register" => $domain['register'],
+            "name" => $domain['name'],
+            "tld" => $domain['tld'],
+            "created_at" => $domain['created_at'],
+            "updated_at" => $domain['updated_at'],
+            "nameserver_1" => $domain['nameserver_1'], 
+            "nameserver_2" => $domain['nameserver_2']
+        ]);
+        $return->assertStatus(302);
+        $return->assertSessionHasErrors([
+            'updated_at' => 'O campo é obrigatório'
+        ]);
+    }
 }
